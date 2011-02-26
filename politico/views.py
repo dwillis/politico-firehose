152c099adbf78cb1a86e893bc61fdcc1a9a9c4e3
Derek Willis <dwillis@nytimes.com>
2011-02-26 07:28:26 -0500

    added basic index view

--- politico/views.py
+++ politico/views.py
@@ -6,22 +6,22 @@ from politico.models import Story, Author
 from google.appengine.api.urlfetch import fetch
 from toolbox import feedparser
 from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
 from django.template.defaultfilters import slugify
 
 feeds = ['http://feeds.politico.com/politico/rss/congress', 'http://www.politico.com/rss/life.xml', 'http://www.politico.com/rss/lobbyists.xml',
 'http://www.politico.com/rss/pitboss.xml', 'http://www.politico.com/rss/politics.xml', 'http://www.politico.com/rss/2012-election.xml', 
-'http://www.politico.com/rss/2012-election-blog.xml', 'http://www.politico.com/rss/click.xml', 'http://www.politico.com/rss/rogersimon.xml',
+'http://www.politico.com/rss/2012-election-blog.xml', 'http://feeds.politico.com/politico/rss/click', 'http://www.politico.com/rss/rogersimon.xml',
 'http://www.politico.com/rss/suitetalk.xml', 'http://www.politico.com/rss/playbook.xml', 'http://www.politico.com/rss/morningscore.xml', 
 'http://www.politico.com/rss/morningmoney.xml', 'http://www.politico.com/rss/politicopulse.xml', 'http://www.politico.com/rss/huddle.xml', 
 'http://www.politico.com/rss/morningenergy.xml', 'http://www.politico.com/rss/morningdefense.xml', 'http://www.politico.com/rss/morningtech.xml',
 'http://www.politico.com/rss/politicopicks.xml', 'http://www.politico.com/rss/Top10Blogs.xml', 'http://www.politico.com/rss/bensmith.xml',
 'http://www.politico.com/rss/davidcatanese.xml', 'http://www.politico.com/rss/laurarozen.xml', 'http://www.politico.com/rss/glennthrush.xml', 
 'http://www.politico.com/rss/onmedia.xml', 'http://www.politico.com/rss/joshgerstein.xml', 'http://www.politico.com/rss/maggiehaberman.xml', 
-'http://www.politico.com/rss/arena/arenatop10.xml', 'http://www.politico.com/rss/Politico44box.xml', 'http://www.politico.com/rss/livepulse.xml']
+'http://www.politico.com/rss/Politico44box.xml', 'http://www.politico.com/rss/livepulse.xml']
 
 def index(request):
-    return 
-    
+    return HttpResponse("nothing to see here. yet.")
 
 
 def fetch_feeds(request):
