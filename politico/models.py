549d598d1a83c21377c0627468826b909e8166cb
Derek Willis <dwillis@nytimes.com>
2011-02-28 21:49:11 -0500

    switched to M2M models and added byline detail view

--- politico/models.py
+++ politico/models.py
@@ -3,11 +3,26 @@ from google.appengine.ext import db
 from pytz.gae import pytz
 from datetime import timedelta
 
+class Author(BaseModel):
+    name = db.StringProperty()
+    slug = db.StringProperty()
+    story_count = db.IntegerProperty()
+    last_updated = db.DateTimeProperty()
+    
+    def __unicode__(self):
+        return self.name
+        
+    def story_list(self):
+        return (x.story for x in self.authorstory_set)
+        
+    def get_absolute_url(self):
+        return "/bylines/%s" % self.slug
+
 class Story(BaseModel):
     title = db.StringProperty()
     link = db.StringProperty()
-    byline = db.StringProperty()
     updated_date = db.DateTimeProperty()
+    multi_byline = db.BooleanProperty()
     
     def __unicode__(self):
         return self.title
@@ -17,15 +32,13 @@ class Story(BaseModel):
     
     def updated_local(self):
         return self.updated_date - timedelta(hours=5)
+        
+    def author_list(self):
+        return (x.author.name for x in self.authorstory_set)
     
-class Author(BaseModel):
-    name = db.StringProperty()
-    slug = db.StringProperty()
-    story_count = db.IntegerProperty()
-    last_updated = db.DateTimeProperty()
-    
-    def __unicode__(self):
-        return self.name
+class AuthorStory(BaseModel):
+    author = db.ReferenceProperty(Author, required=True, collection_name="stories")
+    story = db.ReferenceProperty(Story, required=True, collection_name="authors")
     
 
     
--- politico/views.py
+++ politico/views.py
@@ -1,7 +1,7 @@
 import StringIO
 import datetime, time
-from google.appengine.api.labs import taskqueue
-from politico.models import Story, Author
+from google.appengine.api import taskqueue
+from politico.models import Story, Author, AuthorStory
 from google.appengine.api.urlfetch import fetch
 from toolbox import feedparser
 from django.http import Http404, HttpResponse, HttpResponseRedirect
@@ -11,8 +11,7 @@ from django.template.defaultfilters import slugify
 
 feeds = ['http://feeds.politico.com/politico/rss/congress', 'http://www.politico.com/rss/life.xml', 'http://www.politico.com/rss/lobbyists.xml',
 'http://www.politico.com/rss/pitboss.xml', 'http://www.politico.com/rss/politics.xml', 'http://www.politico.com/rss/2012-election.xml', 
-'http://www.politico.com/rss/2012-election-blog.xml', 'http://feeds.politico.com/politico/rss/click', 'http://www.politico.com/rss/rogersimon.xml',
-'http://www.politico.com/rss/suitetalk.xml', 'http://www.politico.com/rss/playbook.xml', 'http://www.politico.com/rss/morningscore.xml', 
+'http://www.politico.com/rss/2012-election-blog.xml', 'http://www.politico.com/rss/rogersimon.xml', 'http://www.politico.com/rss/suitetalk.xml', 'http://www.politico.com/rss/playbook.xml', 'http://www.politico.com/rss/morningscore.xml', 
 'http://www.politico.com/rss/morningmoney.xml', 'http://www.politico.com/rss/politicopulse.xml', 'http://www.politico.com/rss/huddle.xml', 
 'http://www.politico.com/rss/morningenergy.xml', 'http://www.politico.com/rss/morningdefense.xml', 'http://www.politico.com/rss/morningtech.xml',
 'http://www.politico.com/rss/politicopicks.xml', 'http://www.politico.com/rss/Top10Blogs.xml', 'http://www.politico.com/rss/bensmith.xml',
@@ -28,6 +27,15 @@ def index(request):
     }
     return direct_to_template(request, 'index.html', context)
 
+def byline_detail(request, slug):
+    author = Author.all().filter('slug =', slug).get()
+    context = {
+        'author' : author,
+        'headline': "Article Archive",
+        'object_list': author.stories,
+    }
+    return direct_to_template(request, 'byline_detail.html', context)
+    
 
 def fetch_feeds(request):
     for url in feeds:
@@ -43,19 +51,24 @@ def update_feed(request):
         story_query = Story.all()
         story = story_query.filter('link =', entry.id).get()
         if not story:
-            story = Story(link = entry.id, title = entry.title, byline = entry.author, updated_date = datetime.datetime.fromtimestamp(time.mktime(entry.updated_parsed)))
-            story.put()
-        authors = story.byline.split(',')
-        for author in authors:
-            author_query = Author.all()
-            a = author_query.filter('slug =', str(slugify(author))).get()
-            if a:
-                a.story_count += 1
-                if story.updated_date > a.last_updated:
-                    a.last_updated = story.updated_date
-                    a.save()
+            authors = entry.author.split(',')
+            if len(authors) > 1:
+                multi_byline = True
             else:
-                a = Author(name = author, slug = str(slugify(author)), story_count = 1, last_updated = story.updated_date)
-                a.put()
-    return HttpResponse('ok!')
+                multi_byline = False
+            story = Story(link = entry.id, title = entry.title, updated_date = datetime.datetime.fromtimestamp(time.mktime(entry.updated_parsed)), multi_byline = multi_byline)
+            story.put()
+            for author in authors:
+                author_query = Author.all()
+                a = author_query.filter('slug =', str(slugify(author))).get()
+                if a:
+                    a.story_count += 1
+                    if story.updated_date > a.last_updated:
+                        a.last_updated = story.updated_date
+                        a.save()
+                else:
+                    a = Author(name = author, slug = str(slugify(author)), story_count = 1, last_updated = story.updated_date)
+                    a.put()
+                AuthorStory(author=a, story=story).put()
 
+    return HttpResponse('ok!')
