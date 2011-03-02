879b11703aa990e792b8a7d512c9675a19a32126
palewire <ben.welsh@gmail.com>
2011-03-01 17:32:29 -0800

    Killed the noisy feeds list and replaced it with a YAML config file we could expand with other meta data later.

--- politico/views.py
+++ politico/views.py
@@ -8,16 +8,8 @@ from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.views.generic.simple import direct_to_template
 from django.template.defaultfilters import slugify
+from politico import get_feeds
 
-feeds = ['http://feeds.politico.com/politico/rss/congress', 'http://www.politico.com/rss/life.xml', 'http://www.politico.com/rss/lobbyists.xml',
-'http://www.politico.com/rss/pitboss.xml', 'http://www.politico.com/rss/politics.xml', 'http://www.politico.com/rss/2012-election.xml', 
-'http://www.politico.com/rss/2012-election-blog.xml', 'http://www.politico.com/rss/rogersimon.xml', 'http://www.politico.com/rss/suitetalk.xml', 'http://www.politico.com/rss/playbook.xml', 'http://www.politico.com/rss/morningscore.xml', 
-'http://www.politico.com/rss/morningmoney.xml', 'http://www.politico.com/rss/politicopulse.xml', 'http://www.politico.com/rss/huddle.xml', 
-'http://www.politico.com/rss/morningenergy.xml', 'http://www.politico.com/rss/morningdefense.xml', 'http://www.politico.com/rss/morningtech.xml',
-'http://www.politico.com/rss/politicopicks.xml', 'http://www.politico.com/rss/Top10Blogs.xml', 'http://www.politico.com/rss/bensmith.xml',
-'http://www.politico.com/rss/davidcatanese.xml', 'http://www.politico.com/rss/laurarozen.xml', 'http://www.politico.com/rss/glennthrush.xml', 
-'http://www.politico.com/rss/onmedia.xml', 'http://www.politico.com/rss/joshgerstein.xml', 'http://www.politico.com/rss/maggiehaberman.xml', 
-'http://www.politico.com/rss/Politico44box.xml', 'http://www.politico.com/rss/livepulse.xml']
 
 def index(request):
     latest_stories = Story.all().order('-updated_date').fetch(25)
@@ -38,8 +30,8 @@ def byline_detail(request, slug):
     
 
 def fetch_feeds(request):
-    for url in feeds:
-        taskqueue.add(url = '/_update_feed/', params = {'url' : url}, method='GET')
+    for feed in get_feeds():
+        taskqueue.add(url = '/_update_feed/', params = {'url' : feed.get("feed_url")}, method='GET')
     return HttpResponse('ok!')
 
 
